330cdd9ddff8f4146a8ce6e09372e4dc5c4f0bfb
Metehan Olgun <metehannolgun@192.168.1.100>
2025-01-18 18:44:25 +0300

    Initial Commit:React Restaurant Website project setup

---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='mt-5'>About</div>
+  )
+}
+
+export default About
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='mt-5'>Home</div>
+  )
+}
+
+export default Home
