fe5b31255b36ced504820fb314c76422f4c3d89e
metehannolgun <metehannolgun@gmail.com>
2025-02-17 18:03:11 +0300

    About sayfası üzerinde değişiklikler yapıldı

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import './About.css'
+import Aboutchef1 from '../utils/chef1.jpg'
+import Aboutchef2 from '../utils/chef2.jpg'
+
 
 const About = () => {
   return (
@@ -10,6 +14,30 @@ const About = () => {
             </h1>
           </div>
         </header>
+        <div className='container my-5'>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+              Aliquid ex, molestias mollitia quod modi molestiae delectus blanditiis magnam? Aperiam cupiditate vero,
+               laboriosam sequi inventore vitae provident adipisci modi libero dolorum sed iste repellat consectetur sint,
+                quod ducimus iusto aspernatur natus officiis architecto? Veniam aut minima obcaecati neque molestiae perspiciatis iste ab laborum,
+                 adipisci, nemo ad, consequatur placeat. Minus, esse maxime.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, ipsa assumenda. 
+              At autem voluptate quos reiciendis quis fugiat eum recusandae dicta molestias excepturi natus maiores doloribus,
+               aliquam facilis minima magnam ab? Illo, ea dolores officiis expedita excepturi mollitia molestiae fugiat distinctio qui cupiditate doloribus possimus deserunt ut, 
+               nemo doloremque ipsa nihil tenetur aut. Doloribus deserunt excepturi saepe repellat illo similique et, impedit numquam animi ipsa nobis vero aliquid fugit, 
+               eum accusantium modi temporibus, suscipit qui aspernatur expedita culpa! Facilis, voluptate!
+            </p>
+            <div className='row'>
+                    <div className='col-lg-6'>
+                        <img src={Aboutchef1} className='img-fluid my-4' alt="" />
+                    </div>
+                    <div className='col-lg-6'>
+                        <img src={Aboutchef2} className='img-fluid my-4' alt="" />
+                    </div>
+                </div>
+        </div>
     </div>
   )
 }
